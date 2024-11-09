bcecf03c7dfbe215a593f41754300a28e9e508bf
dev-ImChaeWon <crown980129@gmail.com>
2024-11-09 14:39:08 +0900

    HomeworkController to RestController

--- src/main/java/com/studyflow/controller/SubjectController.java
+++ src/main/java/com/studyflow/controller/SubjectController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.studyflow.dto.SubjectDTO;
 import com.studyflow.service.SubjectService;
@@ -24,6 +25,13 @@ public class SubjectController {
 	@GetMapping("/api/subject")
 	public ResponseEntity<List<SubjectDTO>> getSubjectName() {
 		List<SubjectDTO> res = subs.getSubjectName();
+		
+		return ResponseEntity.status(200).body(res);
+	}
+	
+	@GetMapping("/api/teacher-subject/{userId}")
+	public ResponseEntity<List<SubjectDTO>> getSubject(@PathVariable("userId") String userId){
+		List<SubjectDTO> res = subs.getSubjectByTeacherId(userId);
 		
 		return ResponseEntity.status(200).body(res);
 	}
