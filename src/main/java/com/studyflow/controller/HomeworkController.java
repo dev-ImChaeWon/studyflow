f92770ef15ce277aefb7aeaf0ba42bad381574e0
j-hye-ji <arang030@naver.com>
2024-11-09 14:23:52 +0900

    숙제 코멘트 수정

--- src/main/java/com/studyflow/controller/HomeworkController.java
+++ src/main/java/com/studyflow/controller/HomeworkController.java
@@ -35,10 +35,16 @@ public class HomeworkController {
 		this.homs = homs;
 	}
 	
+	// 숙제 수정 API (코멘트 수정)
+	@PostMapping("/api/homework-comment")
+	public HomeworkDTO updateCommentHomework(@RequestBody HomeworkDTO homeworkDTO) {
+		return homs.updateCommentHomework(homeworkDTO);
+	}
+	
 	// 숙제 수정 API (완료된 페이지 수 수정)
-    @PostMapping("/api/homework-update")
-    public HomeworkDTO updateHomework(@RequestBody HomeworkDTO homeworkDTO) {
-        return homs.updateHomework(homeworkDTO);
+    @PostMapping("/api/homework-completedpage")
+    public HomeworkDTO updatePageHomework(@RequestBody HomeworkDTO homeworkDTO) {
+        return homs.updateCompletedHomework(homeworkDTO);
     }
 	
 	// 학생 숙제 생성 API
